ba34c4051fd506a5af6a1a881fb81ca13de34d84
Peter Wooley <peter@peterwooley.com>
2011-02-24 15:31:51 -0800

    No time to figure out, but having Node's built-in HTTP server listen on localhost doesn't allow it to be accessed remotely. Baking in the Linode IP for now.

--- hello/server.js
+++ hello/server.js
@@ -7,5 +7,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(port, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:'+port+'/');
+}).listen(port, "173.230.154.161");
+console.log('Server running at http://173.230.154.161:'+port+'/');
